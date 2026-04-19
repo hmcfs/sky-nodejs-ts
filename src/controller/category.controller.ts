2ef125c755938a336999b3c344f179b9b9aa933f
h123mc <16216588+h123mc@user.noreply.gitee.com>
2026-04-19 18:04:50 +0800

    分类模块--crud

--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
-import { getListSev } from '../service/category.service';
-import type { CategoryPage } from '../types/category.type';
+import { getListSev, addSev, updateSev, deleteSev } from '../service/category.service';
+import type { CategoryPage, CategoryCreate } from '../types/category.type';
+import { getUserId } from '../utils/jwt';
+import type { UpdateParams } from '../types/category.type';
+//条件查询
 export const getList = async (req: Request, res: Response) => {
   try {
     const { page, pageSize, name, type } = req.query;
@@ -11,6 +14,41 @@ export const getList = async (req: Request, res: Response) => {
       type,
     } as CategoryPage);
     res.success(result);
+  } catch (e) {
+    res.fail(e);
+  }
+};
+//修改
+export const update = async (req: Request, res: Response) => {
+  try {
+    const [affectedCount] = await updateSev(
+      String(req.headers.token || ''),
+      req.body as UpdateParams
+    );
+    if (affectedCount > 0) res.success({ affectedCount });
+    else res.fail('数据未发生更改');
+  } catch (e) {
+    res.fail(e);
+  }
+};
+//新增
+export const add = async (req: Request, res: Response) => {
+  try {
+    const result = await addSev(String(req.headers.token) || '', req.body as CategoryCreate);
+    if (result) {
+      res.success();
+    } else res.fail('数据添加失败');
+  } catch (e) {
+    res.fail(e);
+  }
+};
+// 删除
+export const del = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.query;
+    if (!id) res.fail('id不能为空');
+    const affectedCount = await deleteSev(Number(id));
+    if (affectedCount > 0) res.success({ affectedCount });
   } catch (e) {
     res.fail(e);
   }
--- src/middleware/response.middleware.ts
+++ src/middleware/response.middleware.ts
@@ -2,26 +2,43 @@ import { Response, Request, NextFunction } from 'express';
 declare global {
   namespace Express {
     interface Response {
-      success(data: any, msg?: string): void;
-      fail(msg: any, code?: number): void;
+      success(data?: any, msg?: string): void;
+      fail(err: any, code?: number): void;
     }
   }
 }
 export function responseMiddleware(req: Request, res: Response, next: NextFunction) {
-  res.success = (data: any, msg = 'success', code = 1) => {
+  res.success = (data: any = null, msg = 'success', code = 1) => {
     res.json({
       code,
       msg,
       data,
     });
   };
-  res.fail = (msg: any, code = 0) => {
-    if (typeof msg !== 'string') msg = msg instanceof Error ? msg.message : '服务器错误';
-    res.json({
-      code,
-      msg,
-      data: null,
-    });
+  res.fail = (err: any, code = 0) => {
+    let msg = '服务器错误';
+
+    // 1.  Sequelize 字段校验错误（格式不对、非空）
+    if (err.name === 'SequelizeValidationError') {
+      msg = err.errors?.[0]?.message || '字段校验失败';
+      code = 400;
+    }
+    // 2.  Sequelize 唯一重复错误
+    else if (err.name === 'SequelizeUniqueConstraintError') {
+      msg = '数据重复：' + (err.errors?.[0]?.message || '已存在相同数据');
+      code = 400;
+    }
+    // 3.  JS 标准错误
+    else if (err instanceof Error) {
+      msg = err.message;
+    }
+    // 4. 字符串
+    else if (typeof err === 'string') {
+      msg = err;
+    }
+
+    console.error('响应失败：', msg);
+    res.json({ code, msg, data: null });
   };
   next();
 }
