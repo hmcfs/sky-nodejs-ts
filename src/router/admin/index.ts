116e1b8cd4280c53f44f2ffc68b7307043ef55e1
h123mc <16216588+h123mc@user.noreply.gitee.com>
2026-04-22 20:17:06 +0800

    登录校验

--- src/router/admin/index.ts
+++ src/router/admin/index.ts
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import employeeRouter from './employee';
 import categoryRouter from './category';
 import mealRouter from './meal';
+import dishRouter from './dish';
+import { authMiddleware } from '../../middleware/auth.middleware';
+
 const adminRouter = Router();
+
 adminRouter.use(loginRouter);
+adminRouter.use(authMiddleware);
 adminRouter.use(employeeRouter);
 adminRouter.use('/category', categoryRouter);
 adminRouter.use('/setmeal', mealRouter);
+adminRouter.use('/dish', dishRouter);
 export default adminRouter;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
-import adminRouter from './admin';
-import userRouter from './user';
-const router = Router();
+import adminRouter from './admin/index';
+import userRouter from './user/index';
+import commonRouter from './common/upload';
 
+const router = Router();
+/**
+ * @tags 管理员模块
+ * @description 管理员相关接口
+ */
 router.use('/admin', adminRouter);
+/**
+ * @tags 用户模块
+ * @description 用户相关接口
+ */
+
 router.use('/user', userRouter);
+router.use(commonRouter);
 export default router;
